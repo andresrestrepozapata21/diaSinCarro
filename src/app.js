80494bfb9e2b1ff60629797c51925d41172f60f1
andresrestrepozapata21 <arestrepoz@uqvirtual.edu.co>
2022-03-21 21:07:14 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,30 @@
+var form = document.getElementById("form");
+var answer = document.getElementById("answer");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("me diste un click");
+
+  let datos = new FormData(form);
+
+  fetch("server/register.php", {
+    method: "POST",
+    body: datos,
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data === "error") {
+        answer.innerHTML = `
+          <div class="alert alert-danger" role="alert">
+                Llena todos los campos
+            </div>
+          `;
+      } else {
+        answer.innerHTML = `
+        <div class="alert alert-primary" role="alert">
+              ${data}
+          </div>
+        `;
+      }
+    });
+});
